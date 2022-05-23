593b404a1eb1bcacf787e31ce5972429b6a724d5
Alex Andrianavalontsalama <alextsiresysat@gmail.com>
2022-05-23 18:59:22 -0400

    Done with button feature

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
+//create container div
 const block = document.createElement("div");
 const b = document.querySelector("body");
 b.appendChild(block);
 block.classList.add('box');
 
+//create button 
 const btn = document.createElement("button");
 b.appendChild(btn);
+btn.style.height = "50px";
+btn.style.width = "100px";
 
-let size;
+btn.textContent = "SIZE"
+//get size from user
+let size = 0;
 btn.addEventListener('click', () => {
-size = prompt("Enter the number of pixel per side:", "16")
+size = prompt("Enter the number of pixel per side:")
 size = parseInt(size, 10);
-});
-for(let i = 0; i < 256; i++){
+let num = size * size;
+let g_size = 950/size;
+
+//make inner divs from user input
+for(let i = 0; i < num ; i++){
 const grid = document.createElement("div");
 block.appendChild(grid);
-grid.classList.add('g');}
+grid.classList.add('g');
+grid.style.width =  g_size + 'px';
+grid.style.height =  g_size + 'px';}
 
+//even for hoover
  const pixes = document.querySelectorAll(".g");
  pixes.forEach((pix) => {
      pix.addEventListener('mouseover', () => {
-         pix.style.cssText = "background-color: blue";
+         pix.style['background-color'] = 'blue';
      });
  });
+});
+
